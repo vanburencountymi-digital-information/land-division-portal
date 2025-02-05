da435fe4c88a53dc425a9dc40d928fa70a9e81e1
DrakeO <drake.olejniczak@gmail.com>
2025-02-05 09:13:43 -0500

    Add test firestore read-write script

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+
+// Initalize Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { db };
